44f57712a43d4b41a45a07bc3ccd868bffa4c583
Matt Klepeis <mattklepeis@gmail.com>
2019-03-08 00:54:06 -0500

    Database gets created on initial container load.

--- Authentication.Service/Config/AuthDatabase.Docker/CreateAuthDatabase.sql
+++ Authentication.Service/Config/AuthDatabase.Docker/CreateAuthDatabase.sql
@@ -24,7 +24,7 @@ GO
 USE AuthDB;
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ApiResources')
 	BEGIN
 		CREATE TABLE [ApiResources] (
@@ -42,7 +42,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'Clients')
 	BEGIN
 		CREATE TABLE [Clients] (
@@ -93,7 +93,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'IdentityResources')
 	BEGIN
 		CREATE TABLE [IdentityResources] (
@@ -113,7 +113,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ApiClaims')
 	BEGIN
 		CREATE TABLE [ApiClaims] (
@@ -126,7 +126,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ApiProperties')
 	BEGIN
 		CREATE TABLE [ApiProperties] (
@@ -140,7 +140,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ApiScopes')
 	BEGIN
 		CREATE TABLE [ApiScopes] (
@@ -158,7 +158,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ApiSecrets')
 	BEGIN
 		CREATE TABLE [ApiSecrets] (
@@ -175,7 +175,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientClaims')
 	BEGIN
 		CREATE TABLE [ClientClaims] (
@@ -189,8 +189,8 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
-           WHERE TABLE_NAME = N'ClientCorsOrgins')
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+           WHERE TABLE_NAME = N'ClientCorsOrigins')
 	BEGIN
 		CREATE TABLE [ClientCorsOrigins] (
 			[Id] int NOT NULL IDENTITY,
@@ -202,7 +202,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientGrantTypes')
 	BEGIN
 		CREATE TABLE [ClientGrantTypes] (
@@ -215,7 +215,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientIdPRestrictions')
 	BEGIN
 		CREATE TABLE [ClientIdPRestrictions] (
@@ -228,7 +228,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientPostLogoutRedirectUris')
 	BEGIN
 		CREATE TABLE [ClientPostLogoutRedirectUris] (
@@ -241,7 +241,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientProperties')
 	BEGIN
 		CREATE TABLE [ClientProperties] (
@@ -255,7 +255,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientRedirectUris')
 	BEGIN
 		CREATE TABLE [ClientRedirectUris] (
@@ -268,7 +268,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientScopes')
 	BEGIN
 		CREATE TABLE [ClientScopes] (
@@ -281,7 +281,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ClientSecrets')
 	BEGIN
 		CREATE TABLE [ClientSecrets] (
@@ -298,7 +298,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'IdentityClaims')
 	BEGIN
 		CREATE TABLE [IdentityClaims] (
@@ -311,7 +311,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'IdentityProperties')
 	BEGIN
 		CREATE TABLE [IdentityProperties] (
@@ -325,7 +325,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'ApiScopeClaims')
 	BEGIN
 		CREATE TABLE [ApiScopeClaims] (
@@ -338,7 +338,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
            WHERE TABLE_NAME = N'DeviceCodes')
 	BEGIN
 		CREATE TABLE [DeviceCodes] (
@@ -354,8 +354,8 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
-           WHERE TABLE_NAME = N'DeviceCodes')
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
+           WHERE TABLE_NAME = N'PersistedGrants')
 	BEGIN
 		CREATE TABLE [PersistedGrants] (
 			[Key] nvarchar(200) NOT NULL,
@@ -370,161 +370,161 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiClaims_ApiResourceId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiClaims_ApiResourceId' 
     AND object_id = OBJECT_ID('[dbo].[ApiClaims]'))
 	BEGIN
 		CREATE INDEX [IX_ApiClaims_ApiResourceId] ON [ApiClaims] ([ApiResourceId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiProperties_ApiResourceId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiProperties_ApiResourceId' 
     AND object_id = OBJECT_ID('[dbo].[ApiProperties]'))
 	BEGIN
 		CREATE INDEX [IX_ApiProperties_ApiResourceId] ON [ApiProperties] ([ApiResourceId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiResources_Name' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiResources_Name' 
     AND object_id = OBJECT_ID('[dbo].[ApiResources]'))
 	BEGIN
 		CREATE UNIQUE INDEX [IX_ApiResources_Name] ON [ApiResources] ([Name]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiScopeClaims_ApiScopeId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiScopeClaims_ApiScopeId' 
     AND object_id = OBJECT_ID('[dbo].[ApiScopeClaims]'))
 	BEGIN
 		CREATE INDEX [IX_ApiScopeClaims_ApiScopeId] ON [ApiScopeClaims] ([ApiScopeId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiScopes_ApiResourceId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiScopes_ApiResourceId' 
     AND object_id = OBJECT_ID('[dbo].[ApiScopes]'))
 	BEGIN
 		CREATE INDEX [IX_ApiScopes_ApiResourceId] ON [ApiScopes] ([ApiResourceId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiScopes_Name' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiScopes_Name' 
     AND object_id = OBJECT_ID('[dbo].[ApiScopes]'))
 	BEGIN
 		CREATE UNIQUE INDEX [IX_ApiScopes_Name] ON [ApiScopes] ([Name]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiSecrets_ApiResourceId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ApiSecrets_ApiResourceId' 
     AND object_id = OBJECT_ID('[dbo].[ApiSecrets]'))
 	BEGIN
 		CREATE INDEX [IX_ApiSecrets_ApiResourceId] ON [ApiSecrets] ([ApiResourceId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientClaims_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientClaims_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientClaims]'))
 	BEGIN
 		CREATE INDEX [IX_ClientClaims_ClientId] ON [ClientClaims] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientCorsOrigins_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientCorsOrigins_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientCorsOrigins]'))
 	BEGIN
 		CREATE INDEX [IX_ClientCorsOrigins_ClientId] ON [ClientCorsOrigins] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientGrantTypes_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientGrantTypes_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientGrantTypes]'))
 	BEGIN
 		CREATE INDEX [IX_ClientGrantTypes_ClientId] ON [ClientGrantTypes] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientIdPRestrictions_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientIdPRestrictions_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientIdPRestrictions]'))
 	BEGIN
 		CREATE INDEX [IX_ClientIdPRestrictions_ClientId] ON [ClientIdPRestrictions] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientPostLogoutRedirectUris_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientPostLogoutRedirectUris_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientPostLogoutRedirectUris]'))
 	BEGIN
 		CREATE INDEX [IX_ClientPostLogoutRedirectUris_ClientId] ON [ClientPostLogoutRedirectUris] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientProperties_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientProperties_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientProperties]'))
 	BEGIN
 		CREATE INDEX [IX_ClientProperties_ClientId] ON [ClientProperties] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientRedirectUris_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientRedirectUris_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientRedirectUris]'))
 	BEGIN
 		CREATE INDEX [IX_ClientRedirectUris_ClientId] ON [ClientRedirectUris] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_Clients_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_Clients_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[Clients]'))
 	BEGIN
 		CREATE UNIQUE INDEX [IX_Clients_ClientId] ON [Clients] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientScopes_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientScopes_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientScopes]'))
 	BEGIN
 		CREATE INDEX [IX_ClientScopes_ClientId] ON [ClientScopes] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientSecrets_ClientId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_ClientSecrets_ClientId' 
     AND object_id = OBJECT_ID('[dbo].[ClientSecrets]'))
 	BEGIN
 		CREATE INDEX [IX_ClientSecrets_ClientId] ON [ClientSecrets] ([ClientId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_IdentityClaims_IdentityResourceId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_IdentityClaims_IdentityResourceId' 
     AND object_id = OBJECT_ID('[dbo].[IdentityClaims]'))
 	BEGIN
 		CREATE INDEX [IX_IdentityClaims_IdentityResourceId] ON [IdentityClaims] ([IdentityResourceId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_IdentityProperties_IdentityResourceId' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_IdentityProperties_IdentityResourceId' 
     AND object_id = OBJECT_ID('[dbo].[IdentityProperties]'))
 	BEGIN
 		CREATE INDEX [IX_IdentityProperties_IdentityResourceId] ON [IdentityProperties] ([IdentityResourceId]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_IdentityResources_Name' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_IdentityResources_Name' 
     AND object_id = OBJECT_ID('[dbo].[IdentityResources]'))
 	BEGIN
 		CREATE UNIQUE INDEX [IX_IdentityResources_Name] ON [IdentityResources] ([Name]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_DeviceCodes_DeviceCode' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_DeviceCodes_DeviceCode' 
     AND object_id = OBJECT_ID('[dbo].[DeviceCodes]'))
 	BEGIN
 		CREATE UNIQUE INDEX [IX_DeviceCodes_DeviceCode] ON [DeviceCodes] ([DeviceCode]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_DeviceCodes_UserCode' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_DeviceCodes_UserCode' 
     AND object_id = OBJECT_ID('[dbo].[DeviceCodes]'))
 	BEGIN
 		CREATE UNIQUE INDEX [IX_DeviceCodes_UserCode] ON [DeviceCodes] ([UserCode]);
 	END
 GO
 
-IF EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_PersistedGrants_SubjectId_ClientId_Type' 
+IF NOT EXISTS (SELECT *  FROM sys.indexes  WHERE name='IX_PersistedGrants_SubjectId_ClientId_Type' 
     AND object_id = OBJECT_ID('[dbo].[PersistedGrants]'))
 	BEGIN
 		CREATE INDEX [IX_PersistedGrants_SubjectId_ClientId_Type] ON [PersistedGrants] ([SubjectId], [ClientId], [Type]);
