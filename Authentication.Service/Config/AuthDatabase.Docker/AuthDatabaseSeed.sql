6d0967f67dac8bd539d017ad7a83b8688fe8bcd2
Matt Klepeis <mattklepeis@gmail.com>
2019-03-09 00:56:55 -0500

    Add AuthDBSeed, Does not auto execute when container loads yet.

---
+++ Authentication.Service/Config/AuthDatabase.Docker/AuthDatabaseSeed.sql
@@ -0,0 +1,96 @@
+﻿USE [AuthDB]
+GO
+
+-------- Add Client 'DemoApp' --------
+
+--If database does not exist, create database.
+IF NOT EXISTS(SELECT 1 FROM dbo.Clients WHERE ClientId = 'DemoApp')
+	BEGIN
+		PRINT 'Add DemoApp Client'
+
+		INSERT INTO [dbo].[Clients]
+				   ([Enabled]
+				   ,[ClientId]
+				   ,[ProtocolType]
+				   ,[RequireClientSecret]
+				   ,[ClientName]
+				   ,[Description]
+				   ,[ClientUri]
+				   ,[LogoUri]
+				   ,[RequireConsent]
+				   ,[AllowRememberConsent]
+				   ,[AlwaysIncludeUserClaimsInIdToken]
+				   ,[RequirePkce]
+				   ,[AllowPlainTextPkce]
+				   ,[AllowAccessTokensViaBrowser]
+				   ,[FrontChannelLogoutUri]
+				   ,[FrontChannelLogoutSessionRequired]
+				   ,[BackChannelLogoutUri]
+				   ,[BackChannelLogoutSessionRequired]
+				   ,[AllowOfflineAccess]
+				   ,[IdentityTokenLifetime]
+				   ,[AccessTokenLifetime]
+				   ,[AuthorizationCodeLifetime]
+				   ,[ConsentLifetime]
+				   ,[AbsoluteRefreshTokenLifetime]
+				   ,[SlidingRefreshTokenLifetime]
+				   ,[RefreshTokenUsage]
+				   ,[UpdateAccessTokenClaimsOnRefresh]
+				   ,[RefreshTokenExpiration]
+				   ,[AccessTokenType]
+				   ,[EnableLocalLogin]
+				   ,[IncludeJwtId]
+				   ,[AlwaysSendClientClaims]
+				   ,[ClientClaimsPrefix]
+				   ,[PairWiseSubjectSalt]
+				   ,[Created]
+				   ,[Updated]
+				   ,[LastAccessed]
+				   ,[UserSsoLifetime]
+				   ,[UserCodeType]
+				   ,[DeviceCodeLifetime]
+				   ,[NonEditable])
+			 VALUES
+				   (1
+				   ,'DemoApp'
+				   ,'oidc'
+				   ,0
+				   ,'Demo Application'
+				   ,'This client for authenticating using a Username and Password'
+				   ,NULL
+				   ,NULL
+				   ,0
+				   ,0
+				   ,0
+				   ,0
+				   ,0
+				   ,1
+				   ,NULL
+				   ,1
+				   ,NULL
+				   ,1
+				   ,0
+				   ,300
+				   ,3600
+				   ,300
+				   ,NULL
+				   ,2592000
+				   ,1296000
+				   ,0
+				   ,1
+				   ,0
+				   ,0
+				   ,0
+				   ,0
+				   ,1
+				   ,NULL
+				   ,NULL
+				   ,GETDATE()
+				   ,NULL
+				   ,NULL
+				   ,NULL
+				   ,NULL
+				   ,300
+				   ,0)
+	END
+GO
--- Authentication.Service/Startup.cs
+++ Authentication.Service/Startup.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Authentication.Service.Extensions;
+﻿using Authentication.Service.Extensions;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using System;
 
 namespace Authentication.Service
 {
